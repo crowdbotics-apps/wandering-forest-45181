97f23ff14d8910f8f83cbc28c80f157cea46a577
Crowdbotics <team@crowdbotics.com>
2023-12-24 01:30:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-forest-45181",
+  url: "https://wandering-forest-45181.botics.co",
+  api: "https://wandering-forest-45181.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
